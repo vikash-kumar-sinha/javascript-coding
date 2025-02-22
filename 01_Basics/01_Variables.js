fe0fcd9d65d55f4599449deb08380c317e22b622
vikash-kumar-sinha <139573594+vikash-kumar-sinha@users.noreply.github.com>
2025-02-22 09:27:04 +0000

    datypes file created

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const cName="vikash"
+let lName="vikash"
+var vName="vikash"
+Nname="vikash"
+
+
+console.table([cName,lName,vName,Nname])
+
+console.table([typeof(cName),typeof(lName),typeof(vName),typeof(Nname)])
+//always use 'const' and 'let'
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,13 @@
+//number => 2^53
+//bigInt
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+//object
+
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
