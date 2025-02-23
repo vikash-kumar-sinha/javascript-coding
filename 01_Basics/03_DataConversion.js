f566ce87c6b3d79730e71f89195c393a91379cd2
vikash-kumar-sinha <139573594+vikash-kumar-sinha@users.noreply.github.com>
2025-02-23 04:55:38 +0000

    data conversion file added

---
+++ 01_Basics/03_DataConversion.js
@@ -0,0 +1,13 @@
+let score ="23abc"
+let numScore=Number(score)// NaN
+score="33" // 33 in number type
+console.log(score);
+//Number Conersion
+//"33" =>33
+//"33abc" =>NaN
+//true =>1/ false=>0
+
+//Boolean Conversion
+//1 =>true / false=> 0
+//"" => false
+//"vikash" => true
