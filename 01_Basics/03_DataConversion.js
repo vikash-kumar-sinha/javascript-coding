268ebe68e1ddbad7f4a07db981fbcc015ec083eb
vikash-kumar-sinha <139573594+vikash-kumar-sinha@users.noreply.github.com>
2025-02-23 05:13:29 +0000

    modified data conversion file

--- 01_Basics/03_DataConversion.js
+++ 01_Basics/03_DataConversion.js
@@ -1,7 +1,7 @@
 let score ="23abc"
 let numScore=Number(score)// NaN
 score="33" // 33 in number type
-console.log(score);
+// console.log(score);
 //Number Conersion
 //"33" =>33
 //"33abc" =>NaN
@@ -11,3 +11,24 @@ console.log(score);
 //1 =>true / false=> 0
 //"" => false
 //"vikash" => true
+
+// operations
+
+// console.log(1+2);//3
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log(1+2+"2");//32
+// console.log("1"+2+2);//122
+// console.log("1"+2+"2");//122
+
+// console.log(+true);//1
+// console.log(-true)//-1
+// console.log(+false);//0
+// console.log(-false);//-0
+
+let a,b,c;
+a=b=c=2+2
+console.table([a,b,c])
+
+
+
