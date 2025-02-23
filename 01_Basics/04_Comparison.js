48259b9d7e17bd2aad82362bc2e563026d40e403
vikash-kumar-sinha <139573594+vikash-kumar-sinha@users.noreply.github.com>
2025-02-23 05:36:31 +0000

    added comparison file

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,18 @@
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+
+// == and >,<,<=,>= works diferently
+// in line 3 null converted to 0 than giving true output
+// in line 2 it is caomparing the value null is not converted to 0 here
+
+// console.log(undefined >0);//false
+// console.log(undefined ==0);//false
+// console.log(undefined >=0);//false
+
+// === =>checks the data type also
+console.log("2"==2);//true
+console.log("2"===2);//false
+
+
+
