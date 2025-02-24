bb4d3a15ccd728178fd4812fa9a98ba7df6f4e26
vikash-kumar-sinha <139573594+vikash-kumar-sinha@users.noreply.github.com>
2025-02-24 07:15:34 +0000

    added number and maths file

---
+++ 01_Basics/06_NumbersAndMaths.js
@@ -0,0 +1,10 @@
+let score=100
+console.log(score);
+
+let newScore= new Number(200)
+console.log(newScore);
+
+console.log(newScore.toString())
+console.log(typeof newScore)
+console.log(newScore.toFixed(1))
+
